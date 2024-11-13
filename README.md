327a64bb93d4fe6b11f295664e72857c9bc171cf
Paul King <paulk@asert.com.au>
2024-11-13 14:27:35 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+Source code for: https://groovy.apache.org/blog/groovy-lucene
+
+Output:
+
+<pre>
+Total documents with hits for content:apache* --> 20 hits
+apache-nlpcraft-with-groovy.adoc: apache nlpcraft
+classifying-iris-flowers-with-deep.adoc: apache commons math, apache spark
+community-over-code-eu-2024.adoc: apache ofbiz, apache commons math, apache ignite
+community-over-code-na-2023.adoc: apache ignite, apache commons numbers, apache commons csv
+detecting-objects-with-groovy-the.adoc: apache mxnet
+fruity-eclipse-collections.adoc: apache commons math
+fun-with-obfuscated-groovy.adoc: apache commons math
+groovy-2-5-clibuilder-renewal.adoc: apache commons cli
+groovy-graph-databases.adoc: apache age, apache hugegraph, apache tinkerpop, apache https
+groovy-list-processing-cheat-sheet.adoc: apache commons collections
+groovy-null-processing.adoc: apache commons collections
+groovy-pekko-gpars.adoc: apache pekko
+groovy-record-performance.adoc: apache commons codec
+handling-byte-order-mark-characters.adoc: apache commons io
+matrix-calculations-with-groovy-apache.adoc: apache commons math, apache commons
+natural-language-processing-with-groovy.adoc: apache opennlp, apache spark
+reading-and-writing-csv-files.adoc: apache commons csv
+solving-simple-optimization-problems-with-groovy.adoc: apache commons math, apache kie
+using-groovy-with-apache-wayang.adoc: apache wayang, apache spark, apache flink, apache commons csv, apache ignite
+whiskey-clustering-with-groovy-and.adoc: apache ignite, apache wayang, apache spark, apache commons csv
+
+apache ignite (17)                       ██████████████████████████████████▏
+apache commons math (16)                 ████████████████████████████████▏
+apache spark (12)                        ████████████████████████▏
+apache mxnet (12)                        ████████████████████████▏
+apache age (11)                          ██████████████████████▏
+apache wayang (10)                       ████████████████████▏
+apache commons collections (7)           ██████████████▏
+apache nlpcraft (5)                      ██████████▏
+apache commons csv (5)                   ██████████▏
+apache pekko (4)                         ████████▏
+apache hugegraph (3)                     ██████▏
+apache tinkerpop (3)                     ██████▏
+apache commons cli (2)                   ████▏
+apache opennlp (2)                       ████▏
+apache ofbiz (1)                         ██▏
+apache commons numbers (1)               ██▏
+apache https (1)                         ██▏
+apache commons codec (1)                 ██▏
+apache commons io (1)                    ██▏
+apache commons (1)                       ██▏
+apache kie (1)                           ██▏
+apache flink (1)                         ██▏
+</pre>
---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,83 @@
+import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.LowerCaseFilter
+import org.apache.lucene.analysis.pattern.PatternTokenizer
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.FieldType
+import org.apache.lucene.document.StringField
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.index.IndexOptions
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.Query
+import org.apache.lucene.search.ScoreDoc
+import org.apache.lucene.search.vectorhighlight.FieldPhraseList
+import org.apache.lucene.search.vectorhighlight.FieldQuery
+import org.apache.lucene.search.vectorhighlight.FieldTermStack
+import org.apache.lucene.store.ByteBuffersDirectory
+
+import static org.codehaus.groovy.util.StringUtil.bar
+
+var analyzer = new ApacheProjectAnalyzer()
+var indexDir = new ByteBuffersDirectory()
+var config = new IndexWriterConfig(analyzer)
+var writer = new IndexWriter(indexDir, config)
+
+var blogBaseDir = '/projects/apache-websites/groovy-website/site/src/site/blog'
+new File(blogBaseDir).traverse(nameFilter: ~/.*\.adoc/) { file ->
+    file.withReader { br ->
+        var document = new Document()
+        var fieldType = new FieldType(stored: true,
+            indexOptions: IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS,
+            storeTermVectors: true,
+            storeTermVectorPositions: true,
+            storeTermVectorOffsets: true)
+        document.add(new Field('content', br.text, fieldType))
+        document.add(new StringField('name', file.name, Field.Store.YES))
+        writer.addDocument(document)
+    }
+}
+writer.close()
+
+var reader = DirectoryReader.open(indexDir)
+var searcher = new IndexSearcher(reader)
+var parser = new QueryParser("content", analyzer)
+
+var query = parser.parse('apache*')
+var results = searcher.search(query, 30)
+println "Total documents with hits for $query --> $results.totalHits"
+
+var storedFields = searcher.storedFields()
+var histogram = [:].withDefault { 0 }
+results.scoreDocs.each { ScoreDoc doc ->
+    var document = storedFields.document(doc.doc)
+    var found = handleHit(doc, query, reader)
+    println "${document.get('name')}: ${found*.replaceAll('\n', ' ').toUnique().join(', ')}"
+    found.each { histogram[it.replaceAll('\n', ' ')] += 1 }
+}
+histogram.sort { e -> -e.value }.each { k, v ->
+    var label = "$k ($v)"
+    println "${label.padRight(40)} ${bar(v * 2, 0, 40, 40)}"
+}
+
+List<String> handleHit(ScoreDoc hit, Query query, DirectoryReader dirReader) {
+    boolean phraseHighlight = Boolean.TRUE
+    boolean fieldMatch = Boolean.TRUE
+    FieldQuery fieldQuery = new FieldQuery(query, dirReader, phraseHighlight, fieldMatch)
+    FieldTermStack fieldTermStack = new FieldTermStack(dirReader, hit.doc, 'content', fieldQuery)
+    FieldPhraseList fieldPhraseList = new FieldPhraseList(fieldTermStack, fieldQuery)
+    fieldPhraseList.phraseList*.termsInfos*.text.flatten()
+}
+
+class ApacheProjectAnalyzer extends Analyzer {
+    private static final String tokenRegex = /(?i)\b(apache\s(commons\s)?(?!(groovy|and|license|software|projects|technologies))\w+)|(?!(apache\s)(\w+))/
+
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName) {
+        var src = new PatternTokenizer(~tokenRegex, 0)
+        var result = new LowerCaseFilter(src)
+        new TokenStreamComponents(src, result)
+    }
+}
